8476f202d7363672c52203c508a12e745768096b
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-22 03:40:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves# code an argument here
   # Your code here
   ["Doc", "Dopey", "Bashful", "Grumpy"]. each_with_index do |index,element|
-      puts "#{index.length} #{elent}"
+      puts "#{index.length+1} #{elent}"
 end
 
 def summon_captain_planet# code an argument here
