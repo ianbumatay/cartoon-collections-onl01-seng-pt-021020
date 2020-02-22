b4cc6e49b5ddd66db368c854ac1542e0f12d563a
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-22 04:39:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,16 +8,20 @@ roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
 
 def summon_captain_planet(captain)# code an argument here
   # Your code here
-   cap_array = captain.split(" ")
-   cap_array.map do |element|
-      puts element + "!"
+   new_array = []
+   captain.map do |element|
+   new_array << "#{element.capitalize} !"
     end
+    new_array
 end
 summon_captain_planet(planeteer_calls = ["earth", "wind", "fire", "water", "heart"])
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(short_words)# code an argument here
   # Your code here
-end
+
+
+  end
+  short_words(["puff", "go", "two"])
 
 def find_the_cheese# code an argument here
   # the array below is here to help
