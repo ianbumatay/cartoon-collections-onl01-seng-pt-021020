39b404bc4b021beb5af652c63033d5c853c79232
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-22 03:49:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarf)# code an argument here
   # Your code here
-  dwarf.each_with_index do |index,element|
-      puts "#{index.length+1} #{element}"
+  dwarf.each_with_index do |element,index|
+      puts "#{element} #{index.length+1}"
     end
 end
 roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
