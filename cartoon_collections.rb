8d10e2aec8ffdebdfe9291e4eea722fa66a72275
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-22 04:20:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,10 +8,10 @@ roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
 
 def summon_captain_planet(captain)# code an argument here
   # Your code here
-
-  new_array = []
-  captain.map do | element|
-    new_array << element.capitalize
+   capitalize_array = captain.capitalize
+   new_array = []
+  capitalize_array.map do | element|
+    new_array << element + "!"
   end
   new_array
 end
